03a9827aa6237f512b491019ce5e6ef8bb7b357b
AhmedSamir193 <ahmed.samir.20250@gmail.com>
2023-06-15 05:15:17 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-  /**
-  * puts a value for task 
-  */
-   puts("Programming is like building a multilingual puzzle");
-   getchar();
-   return 0;
-
+/**
+* puts a value for task 
+*/
+puts("Programming is like building a multilingual puzzle");
+getchar();
+return (0);
 }
