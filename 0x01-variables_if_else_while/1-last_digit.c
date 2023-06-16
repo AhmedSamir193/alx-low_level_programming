ff61539fd329c42e4b7d21b1d56478236b48626e
AhmedSamir193 <ahmed.samir.20250@gmail.com>
2023-06-16 08:14:52 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ digit = n % 10;
 
 if (digit > 5)
 
-printf("Last digit of %i is %i and is greater than 5\n", n, n % digit);
+printf("Last digit of %i is %i and is greater than 5\n", n, digit);
 
 else if (digit == 0)
 
-printf("Last digit of %i is %i and is less than 6 not 0\n", n, n % digit);
+printf("Last digit of %i is %i and is less than 6 not 0\n", n, digit);
 
 else if (digit < 6 && digit != 0)
 
-printf("Last digit of %i is %i and is 0\n", n, n % digit);
+printf("Last digit of %i is %i and is 0\n", n, digit);
 
 return (0);
 }
