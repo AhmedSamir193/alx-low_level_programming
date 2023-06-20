c2b45b9b56b61300852b708b36fc0a54faffbb3c
AhmedSamir193 <ahmed.samir.20250@gmail.com>
2023-06-20 05:18:57 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,12 @@
 /**
  * _isalpha - checks if character is alphabetic.
  * @c: The character will be checked.
+ *
  * Return: 1 if c is a letter, lowercase or uppercase, Returns 0 otherwise.
 */
 int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z') ||
-((c >= 'A' && c <= 'Z'))
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 return (1);
 else
 return (0);
