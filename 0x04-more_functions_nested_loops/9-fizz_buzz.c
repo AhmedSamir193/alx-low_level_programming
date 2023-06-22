d0b624f021af8d8a1da0211009922fd3949a3701
AhmedSamir193 <ahmed.samir.20250@gmail.com>
2023-06-22 04:20:04 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,9 +14,9 @@ int n;
 for (n = 1; n <= 100; n++)
 {
 if ((n % 3) == 0 && (n % 5) == 0)
-printf("fizzBuzz");
+printf("FizzBuzz");
 else if ((n % 3) == 0)
-printf("fizz");
+printf("Fizz");
 else if ((n % 5) == 0)
 printf("Buzz");
 else
