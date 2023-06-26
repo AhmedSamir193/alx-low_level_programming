41b46e0c8ab5ab8d110689d39a6a3d7443960bfd
AhmedSamir193 <ahmed.samir.20250@gmail.com>
2023-06-26 07:39:41 -0700

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: the string
+ *
+*/
+void puts2(char *str)
+{
+int j;
+int k = 0;
+	
+while (str[k] != '\0')
+{
+k++;
+}
+for (j = 0; j < k; j += 2)
+{
+_putchar(str[j]);
+}
+_putchar('\n');
+}
